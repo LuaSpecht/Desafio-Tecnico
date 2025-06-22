36b202e4392743919fc62f3370b24d4b336a1fbd
Luana Specht <spechtdeveloper@gmail.com>
2025-06-22 19:44:13 -0300

    feat: add form and information section

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const dropdownButton = document.querySelector('.user-profile')
 const dropdownMenu = document.querySelector('.dropdown-menu')
 const dropdownArrow = document.querySelector('.arrow')
+const userName = document.querySelectorAll('.username')
+const fullName = document.querySelector('#fullname')
+const email = document.querySelector('#user-email')
+const cellphoneNumber = document.querySelector('#phone')
 
 dropdownButton.addEventListener('click', () =>{
   dropdownMenu.classList.toggle('open')
